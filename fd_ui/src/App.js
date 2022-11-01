52c3aaab145370881663f4e29ad26a2ea8c2a87e
GlassCol <coleglass18@outlook.com>
2022-11-01 14:52:46 -0500

    Reformatted display ...toDo Implement links

--- fd_ui/src/App.js
+++ fd_ui/src/App.js
@@ -2,6 +2,7 @@ import React from 'react';
 import Home from './Home';
 import Login from './Login';
 import { BrowserRouter as Router, Route, Routes } from 'react-router-dom'
+import RestaurantPage from './components/RestaurantPage';
 
 function App() {
   return (
@@ -9,6 +10,7 @@ function App() {
     <Routes>
           <Route path='/' element={<Login />} />
           <Route path='/Home' element={<Home />} />
+          <Route path='/RestaurantPage/:id' element={<RestaurantPage />} />
     </Routes>
   </Router>
   );
